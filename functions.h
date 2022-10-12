4239c7de0b7636c60a4628fd9a6e20c7054ce8b6
mseeam <mseeam99@gmail.com>
2022-10-12 12:31:08 -0500

    functions.h

---
+++ functions.h
@@ -0,0 +1,9 @@
+/*********************
+Name: Mohammad Seeam
+Assignment 2: Hello World
+Purpose: added a prototype inside the #ifndef directive
+**********************/
+#ifndef COMMANDLINE_FUNCTIONS_H
+#define COMMANDLINE_FUNCTIONS_H
+int string_length(const char*);
+#endif //COMMANDLINE_FUNCTIONS_H
