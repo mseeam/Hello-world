0b14de723c30c81c67a52f65df9500e2285af942
mseeam <mseeam99@gmail.com>
2022-10-12 12:31:52 -0500

    main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+/*********************
+Name: Mohammad Seeam
+Assignment 2: Hello World
+Purpose: included functions.h
+**********************/
+
+#ifndef COMMANDLINE_MAIN_H
+#define COMMANDLINE_MAIN_H
+#include <iostream>
+#include "functions.h"
+using std::cout;
+using std::endl;
+#endif //COMMANDLINE_MAIN_H
+
